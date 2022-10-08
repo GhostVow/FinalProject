91aa557e451d724d11cca6b1341876dee67d1a2e
turkogluhakan613@gmail.com <turkogluhakan613@gmail.com>
2022-10-09 01:27:19 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Business Code
+            //Yetkisi var mı?
+            return _productDal.GetAll();
+
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _productDal.GettAllByCategory(categoryId);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService productService = new ProductManager(new InMemoryProductDal());
+
+            var products = productService.GetAll();
+
+            products.ForEach(p => Console.WriteLine(p.ProductName));
+
+        }
+    }
+}
